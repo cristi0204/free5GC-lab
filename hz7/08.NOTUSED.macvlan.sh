cb044be57343efbfe4bb0d82d9b7d343d489f8a5
Peter Styk <peter@neux.io>
2022-10-19 13:52:06 +0200

    added prerequisites and experiment dir

--- hz7/08.NOTUSED.macvlan.sh
+++ hz7/08.NOTUSED.macvlan.sh
@@ -2,22 +2,16 @@ cat <<EOF | kubectl create -f -
 apiVersion: "k8s.cni.cncf.io/v1"
 kind: NetworkAttachmentDefinition
 metadata:
-  name: macvlan-conf
+  name: macvlan
 spec:
   config: '{
-      "cniVersion": "0.3.0",
-      "type": "macvlan",
-      "master": "eth0",
-      "mode": "bridge",
+    "cniVersion": "0.3.1",
+    "name": "macvlan-main",
+    "type": "macvlan",
+    "mode": "bridge",
+    "master": "eth0",
       "ipam": {
-        "type": "host-local",
-        "subnet": "172.17.9.0/24",
-        "rangeStart": "172.17.9.240",
-        "rangeEnd": "172.17.9.250",
-        "routes": [
-          { "dst": "0.0.0.0/0" }
-        ],
-        "gateway": "172.17.9.1"
+            "type": "static"
       }
     }'
 EOF
