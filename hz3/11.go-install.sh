e0dd459a63b53546be98d8a71fb51c5a6c2e4c47
Peter Styk <peter@neux.io>
2022-10-20 10:31:11 +0200

    modified go-install vswitch

--- hz3/11.go-install.sh
+++ hz3/11.go-install.sh
@@ -2,6 +2,6 @@ cd /
 wget https://go.dev/dl/go1.17.linux-amd64.tar.gz
 tar -xvzf go1.17.linux-amd64.tar.gz
 ls
-cp go /usr/local/
+cp -r go /usr/local/
 export PATH=$PATH:/usr/local/go/bin
 go version
