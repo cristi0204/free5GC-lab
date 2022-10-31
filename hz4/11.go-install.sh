1600db9cd3fdeaf7e04cd827cb6892e0126fc696
Peter Styk <peter@styk.tv>
2022-10-31 09:34:16 +0000

    update to go installator

--- hz4/11.go-install.sh
+++ hz4/11.go-install.sh
@@ -1,7 +1,10 @@
-cd /
-wget https://go.dev/dl/go1.17.linux-amd64.tar.gz
-tar -xvzf go1.17.linux-amd64.tar.gz
-ls
-cp go /usr/local/
-export PATH=$PATH:/usr/local/go/bin
-go version
+
+mkdir -p ~/tmp/go
+pushd ~/tmp/go
+    wget https://go.dev/dl/go1.17.linux-amd64.tar.gz
+    tar -xvzf go1.17.linux-amd64.tar.gz
+    ls
+    mv go /usr/local/
+    export PATH=$PATH:/usr/local/go/bin
+    go version
+popd
