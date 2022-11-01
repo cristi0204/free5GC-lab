8efd8abde6028cf91c791e5dd2f8f83bacfa16b7
Peter Styk <peter@neux.io>
2022-11-01 10:01:36 +0100

    update to plugin build

--- hz4/12.plugins.sh
+++ hz4/12.plugins.sh
@@ -1,17 +1,19 @@
-
+export PATH=$PATH:/usr/local/go/bin
 mkdir -p /opt/cni/bin/
 
+figlet clone plugins
 pushd ~/git
     git clone https://github.com/containernetworking/plugins.git containernetworking_plugins
 popd
 
+figlet build plugins
 pushd ~/git/containernetworking_plugins
-    ls
+    ls -al
     bash ./build_linux.sh
 popd
 
+figlet copy static
 pushd ~/git/containernetworking_plugins/bin
-    ls
-    sleep 2s
     cp -r static /opt/cni/bin/
+    ls -al /opt/cni/bin/
 popd
